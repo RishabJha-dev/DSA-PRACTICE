1f80d28182f56611961dbb4b39880842f8b057b7
Rishab Kumar Jha <rishabkumarjha180@gmail.com>
2026-09-26 20:44:33 +0530

    Pyramid pattern printing
    Removed commented-out code and cleaned up the pyramid printing logic.

--- Pattern_Printing/Pyramid.cpp
+++ Pattern_Printing/Pyramid.cpp
@@ -1,20 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    // int n,v=0;
-    // cin>>n;
-    // for (int i=1;i<=n;i++){
-    //     for (int j=1;j<n+1-i;j++){
-    //         cout<<"  ";
-    //     }
-    //     for (int j=1;j<=(i+v);j++){
-    //         cout<<"* ";
-    //     }
-    //     v+=1;
-    //     cout<<endl;
-    // }
- //method 2 nsp,nst
-
     int n,v=0;
     cin>>n;
     int nsp= n-1,nst = 1;
@@ -29,6 +15,4 @@ int main(){
         nst +=2;
         cout<<endl;
     }
-
-
 }
